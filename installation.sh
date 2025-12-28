bb4d8394ed232d573df651b84b2b25beddefb6e3
ian <ian.page38@gmail.com>
2025-12-28 13:16:24 +0100

    update

--- installation.sh
+++ installation.sh
@@ -1,3 +1,4 @@
+pip install --upgrade pip
 pip install xformers==0.0.27.post2 --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
 pip install --no-build-isolation git+https://github.com/nerfstudio-project/gsplat.git@0b4dddf04cb687367602c01196913cde6a743d70
