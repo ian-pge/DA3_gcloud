0a246bf778f9635ff6745301e64f3a78eeb7a912
ian <ian.page38@gmail.com>
2025-12-28 16:54:04 +0100

    update

--- installation.sh
+++ installation.sh
@@ -1,7 +1,11 @@
-pip install --upgrade pip
-pip install xformers==0.0.27.post2 --index-url https://download.pytorch.org/whl/cu121
-pip install -r requirements.txt
+git clone --recurse-submodules git@github.com:ian-pge/DA3_gcloud.git
+python -m pip install --upgrade pip
+python -m pip install --ignore-installed "blinker>=1.9.0"
+cd DA3_gcloud/
+pip install xformers==0.0.24 --index-url https://download.pytorch.org/whl/cu121
+pip install -r requirements_docker.txt
 pip install --no-build-isolation git+https://github.com/nerfstudio-project/gsplat.git@0b4dddf04cb687367602c01196913cde6a743d70
+cd Depth-Anything-3
 pip install -e .
 export HF_HOME=/workspace/hf_cache
 
